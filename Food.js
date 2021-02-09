f236b9d5963a5e59a1d95423f3a4e328c5a93b6e
ShreyaAmmanagi <74234689+ShreyaAmmanagi@users.noreply.github.com>
2021-02-09 21:54:03 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,39 @@
+class food{
+    constructor(){
+    this.foodStock = 0
+    this.image = loadImage("Images/Milk.png");
+    }
+    
+    updateFoodStock(foodstock){
+    this.foodStock = foodstock
+    }
+
+    deductFood(){
+    if(this.foodStock>0)
+     this.foodStock = this.foodStock-1
+    }
+
+    getFoodStock(){
+    return this.foodStock
+    }   
+
+    display(){
+        var x = 80,y=100
+    if(this.foodStock!=0){
+        for(var i = 0; i<this.foodStock; i++){
+            if(i%10 == 0){
+                x = 80
+                y = y+50
+            
+            }
+            imageMode(CENTER)
+            image(this.image,x,y,50,50);
+            x = x+30
+        }
+          
+    }
+}   
+   
+   
+
+}
